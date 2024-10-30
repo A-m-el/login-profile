06a0607fcfe0d1ef83454485164ebad9a4a8546a
A-m-el <guelfoutamel8@gmail.com>
2024-10-30 08:40:19 +0100

    feat: add the validate_on_submit method to the register form

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,20 @@
-from flask import  render_template
+from flask import  flash, redirect, render_template, request, session, url_for
 from app import app
 from app.forms import RegisterForm
+from app.models import User
 
 @app.route('/')
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-        # if form.validate_on_submit():  # This checks if the form submission is valid
-        #     # Handle successful form submission here, e.g., save data
-        #     print("User Added yew!")
-        #     return redirect(url_for('register'))
+    if form.validate_on_submit():  # This checks if the form submission is valid
+    # Handle successful form submission here, e.g., save data
+        # print("User Added yew!")
+        # if (request.method == 'POST'):
+        #     user = User(form.name.data, form.email.data,
+        #                 form.password.data )
+        #     session.add(user)
+        #     # flash('thanks for registering')
+        return redirect(url_for('register'))
     return render_template('register.html', form=form) 
 
