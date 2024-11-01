e9c3775ca16b340c39c77023ec48a00762518a16
A-m-el <guelfoutamel8@gmail.com>
2024-11-01 21:23:14 +0100

    add some changes

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,20 @@
 from dataclasses import field
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, StringField, SubmitField, ValidationError, EqualTo
+from wtforms import EmailField, PasswordField, StringField, SubmitField
+from wtforms.validators import  EqualTo, DataRequired, EqualTo
+
+
 
 class RegisterForm(FlaskForm):
-    name = StringField('Name') #, validators=[DataRequired()]
-    email = EmailField('Email Address')
+    name = StringField('Name', validators=[DataRequired()]) #, validators=[DataRequired()]
+    email = EmailField('Email Address', validators=[DataRequired()])
     
     # def validate_password(self, field):
     #     if len(field.data < 8):
     #         raise ValidationError('Passwors must be at least 8 characters')
-    password = PasswordField('Password') #, validate_password(password)
-    password2 = PasswordField(
-'Repeat Password', validators=[ EqualTo('password')])
+    password = PasswordField('Password', validators=[DataRequired()]) #, validate_password(password)
+    password2 = PasswordField( 'Repeat Password', 
+                              validators=[EqualTo('password', message='Passwords must match')])
     register = SubmitField('Register')    
      
     
