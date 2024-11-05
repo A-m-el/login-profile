c759fb46cf89ef1b241fafc0e4b997ebb45c9a3d
A-m-el <guelfoutamel8@gmail.com>
2024-11-05 09:20:41 +0100

    feat: create the base.html template

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,20 @@
-from flask import flash, redirect, render_template, request, url_for
+from flask import flash, redirect, render_template, url_for
 from app import app, db
 from app.forms import RegisterForm
 from app.models import User
 
-@app.route('/' , methods=['GET', 'POST'])
+@app.route('/' )#, methods=['GET', 'POST']
 @app.route('/register', methods=['GET','POST'])
 def register():
     form = RegisterForm()
 
     # This checks if the form submission is valid and method is POST
-    if form.validate_on_submit():
-        print("Form is valid")
+    if form.validate_on_submit(): # i need to know what is this
         user = User(name=form.name.data, email=form.email.data)
-        user.set_password(form.password.data)  # Ensure `set_password` is implemented correctly
+        user.set_password(form.password.data)  
         db.session.add(user)
         db.session.commit()
         
-        flash('Congratulations, you are now a registered user!')
+        flash('Congratulations, you are now a registered user!') # this is  not working
         return redirect(url_for('register'))
     return render_template('register.html', title='Register', form=form)
--- config.py
+++ config.py
@@ -1,13 +1,3 @@
-# import os
-# basedir = os.path.abspath(os.path.dirname(__file__)) # this is an absolute path for where the config.py exist
-
-# class Config(object):
-# # ...
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-#     'sqlite:///' + os.path.join(basedir, 'app.db') # This is a fallback value
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
