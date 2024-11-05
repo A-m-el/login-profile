511b05d284514459b27b34a0b212b16305683ea6
A-m-el <guelfoutamel8@gmail.com>
2024-11-05 10:32:01 +0100

    feat: add password length validator

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,23 @@
 from dataclasses import field
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, StringField, SubmitField
+from wtforms import EmailField, PasswordField, StringField, SubmitField, ValidationError
 from wtforms.validators import  EqualTo, DataRequired, EqualTo
+from wtforms import validators
 
 
 
 class RegisterForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()]) #, validators=[DataRequired()]
     email = EmailField('Email Address', validators=[DataRequired()])
-    
     # def validate_password(self, field):
     #     if len(field.data < 8):
     #         raise ValidationError('Passwors must be at least 8 characters')
-    password = PasswordField('Password', validators=[DataRequired()]) #, validate_password(password)
+    password = PasswordField('Password', [
+                validators.data_required(message='password is required'),
+                validators.Length(message='password should be at least 8 char', min=8)
+                ])
+                            #  validators=[DataRequired(), validators.Length(min=8, message='Too short')] ) #, validate_password(password)
     password2 = PasswordField( 'Repeat Password', 
                               validators=[EqualTo('password', message='Passwords must match')])
     register = SubmitField('Register')    
